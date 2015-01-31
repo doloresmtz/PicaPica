5d6c949302c959c739057a28aeaadca016673271
“Dolores <doloresmtz@gmail.com>
2015-01-31 11:03:45 -0600

    Initial Commit

---
+++ PicaPica/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PicaPica
+//
+//  Created by SantiagoDls on 31/01/15.
+//  Copyright (c) 2015 ConstruApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PicaPica/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PicaPica
+//
+//  Created by SantiagoDls on 31/01/15.
+//  Copyright (c) 2015 ConstruApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
